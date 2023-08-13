58476f45ba7509d21226db0888d9c6d4cd1f99cd
Kannan Kirishikesan <kannan@wso2.com>
2023-08-13 11:04:40 +0530

    added splashscreen

--- TimeTheEgg/ViewController.swift
+++ TimeTheEgg/ViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let softTime : Int = 5
+    let mediumTime : Int = 7
+    let hardTime : Int = 12
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        print(sender.titleLabel?.text)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
