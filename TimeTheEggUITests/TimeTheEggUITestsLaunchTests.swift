f9dc059043cdbab318ce633bbe9a22a6de665014
Kannan Kirishikesan <kannan@wso2.com>
2023-08-13 08:38:55 +0530

    Initial Commit

---
+++ TimeTheEggUITests/TimeTheEggUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TimeTheEggUITestsLaunchTests.swift
+//  TimeTheEggUITests
+//
+//  Created by Kirishikesan on 2023-08-13.
+//
+
+import XCTest
+
+final class TimeTheEggUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
